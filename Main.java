782ce6dfe4c152807e81bf2a078b843d23acd0eb
Chaiwat <127535021+Amtheone123@users.noreply.github.com>
2025-04-10 18:20:22 +0700

    Create Main.java
    Am write very bad code for you

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class BuggyProgram {
+public static void main(String args) {
+    int x = "hello"; // wrong type
+
+    for (int i = 0; i < 10; i--); { // infinite loop + misplaced semicolon
+        System.out.println("Number is " + i) // missing semicolon
+    }
+
+    if (x = 5) // = instead of ==
+        System.out.println("x is 5");
+    else
+        System.out.println("x not 5");
+
+    int[] nums = new int[5];
+    nums[10] = 100; // out of bounds
+
+    String name = null;
+    System.out.println(name.length()); // null pointer
+
+    int z = divide(5, 0); // division by zero
+    System.out.println("Result is: " + z);
+}
+
+public static int divide(int a, int b) {
+    return a / b
+}
