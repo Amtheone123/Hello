869be9b43978dfdc7cd9d71a6d56bd46cf3e29f5
Chaiwat <127535021+Amtheone123@users.noreply.github.com>
2025-04-10 18:46:04 +0700

    Update Main.java
    I fix it already

--- Main.java
+++ Main.java
@@ -1,26 +1,27 @@
 public class BuggyProgram {
-public static void main(String args) {
-    int x = "hello"; // wrong type
+    public static void main(String[] args) {
+        int x = 5; // fixed: use an integer
 
-    for (int i = 0; i < 10; i--); { // infinite loop + misplaced semicolon
-        System.out.println("Number is " + i) // missing semicolon
+        for (int i = 0; i < 10; i++) { // fixed: i++, removed semicolon
+            System.out.println("Number is " + i); // fixed: added semicolon
+        }
+
+        if (x == 5) // fixed: == instead of =
+            System.out.println("x is 5");
+        else
+            System.out.println("x not 5");
+
+        int[] nums = new int[5];
+        nums[4] = 100; // fixed: use a valid index
+
+        String name = "Default"; // fixed: avoid null
+        System.out.println(name.length()); // safe to call length()
+
+        int z = divide(5, 1); // fixed: no division by zero
+        System.out.println("Result is: " + z);
     }
 
-    if (x = 5) // = instead of ==
-        System.out.println("x is 5");
-    else
-        System.out.println("x not 5");
-
-    int[] nums = new int[5];
-    nums[10] = 100; // out of bounds
-
-    String name = null;
-    System.out.println(name.length()); // null pointer
-
-    int z = divide(5, 0); // division by zero
-    System.out.println("Result is: " + z);
-}
-
-public static int divide(int a, int b) {
-    return a / b
+    public static int divide(int a, int b) {
+        return a / b; // fixed: added semicolon
+    }
 }
